186e2f28552987efab876f37b337aedfa65623bb
Ronald Benjamin <dodo.ben.rb@gmail.com>
2023-11-24 14:12:28 +0530

    update .gitignore to exclude postgres-data

--- 02_web-testing/cmd/web/main.go
+++ 02_web-testing/cmd/web/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -9,19 +11,45 @@ import (
 
 type application struct {
 	Session *scs.SessionManager
+	DB      *sql.DB
+	DSN     string
 }
 
+const (
+	host            = "localhost"
+	user            = "postgres"
+	password        = "postgres"
+	dbname          = "users"
+	sslmode         = "disable"
+	timezone        = "UTC"
+	connect_timeout = 5
+	port            = 5432
+)
+
 func main() {
-	// setup am app config, app will become the reciever for sharing information
+
+	dsn := fmt.Sprintf("host=%s port=%v user=%s password=%s dbname=%s sslmode=%s timezone=%s connect_timeout=%v", host, port, user, password, dbname, sslmode, timezone, connect_timeout)
+	fmt.Println(dsn)
+	//	setup an app config, app will become the reciever for sharing information
 	app := application{
 		Session: getSession(),
+		DSN:     dsn,
 	}
+	// flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=users sslmode=disable timezone=UTC connect_timeout=5", "Posgtres connection")
+	// flag.Parse()
+	fmt.Println(app.DSN)
+	conn, err := app.connectToDatabase()
+	if err != nil {
+		log.Fatal(err)
+	}
+	app.DB = conn
+
 	// get application routes
 	mux := app.routes()
 	// get a session manager
 	//app.Session = getSession()
 	log.Println("Starting server on port 8080...")
-	err := http.ListenAndServe(":8080", mux)
+	err = http.ListenAndServe(":8080", mux)
 	if err != nil {
 		log.Fatal(err)
 	}
