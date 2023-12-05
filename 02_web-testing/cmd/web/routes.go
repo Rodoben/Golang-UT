918f1558b84f52a683b1db3a2bc134dca4470ce4
Ronald Benjamin <dodo.ben.rb@gmail.com>
2023-12-05 18:49:48 +0530

    upload profile pic

--- 02_web-testing/cmd/web/routes.go
+++ 02_web-testing/cmd/web/routes.go
@@ -15,12 +15,14 @@ func (app *application) routes() http.Handler {
 
 	mux.Get("/", app.Home)
 	mux.Post("/login", app.Login)
-
 	mux.Route("/user", func(mux chi.Router) {
 		mux.Use(app.auth)
 		mux.Get("/profile", app.Profile)
+		mux.Post("/upload-profile-pic", app.UploadProfilePic)
 	})
-	fileServer := http.FileServer(http.Dir("/static/"))
+	// static assets
+	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 	return mux
 }
--- 02_web-testing/pkg/repository/dbrepo/users_postgres.go
+++ 02_web-testing/pkg/repository/dbrepo/users_postgres.go
@@ -65,11 +65,13 @@ func (m *PostgresDBRepo) GetUser(id int) (*data.User, error) {
 
 	query := `
 		select 
-			id, email, first_name, last_name, password, is_admin, created_at, updated_at 
+			u.id, u.email, u.first_name, u.last_name, u.password, u.is_admin, u.created_at, u.updated_at,
+			coalesce(ui.file_name, '')
 		from 
-			users 
+			users u
+			left join user_images ui on (ui.user_id = u.id)
 		where 
-		    id = $1`
+		    u.id = $1`
 
 	var user data.User
 	row := m.DB.QueryRowContext(ctx, query, id)
@@ -83,6 +85,7 @@ func (m *PostgresDBRepo) GetUser(id int) (*data.User, error) {
 		&user.IsAdmin,
 		&user.CreatedAt,
 		&user.UpdatedAt,
+		&user.ProfilePic.FileName,
 	)
 
 	if err != nil {
@@ -98,11 +101,13 @@ func (m *PostgresDBRepo) GetUserByEmail(email string) (*data.User, error) {
 	defer cancel()
 	query := `
 		select 
-			id, email, first_name, last_name, password, is_admin, created_at, updated_at 
+		    u.id, u.email, u.first_name, u.last_name, u.password, u.is_admin, u.created_at, u.updated_at,
+			coalesce(ui.file_name, '')
 		from 
-			users 
+			users u
+			left join user_images ui on (ui.user_id = u.id)
 		where 
-		    email = $1`
+		    u.email = $1`
 
 	var user data.User
 	row := m.DB.QueryRowContext(ctx, query, email)
@@ -116,6 +121,7 @@ func (m *PostgresDBRepo) GetUserByEmail(email string) (*data.User, error) {
 		&user.IsAdmin,
 		&user.CreatedAt,
 		&user.UpdatedAt,
+		&user.ProfilePic.FileName,
 	)
 	if err != nil {
 		return nil, err
@@ -218,10 +224,16 @@ func (m *PostgresDBRepo) ResetPassword(id int, password string) error {
 func (m *PostgresDBRepo) InsertUserImage(i data.UserImage) (int, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
 	defer cancel()
+
+	stmt := `delete from user_images where user_id = $1`
+	_, err := m.DB.ExecContext(ctx, stmt, i.UserID)
+	if err != nil {
+		return 0, err
+	}
 	var newID int
-	stmt := `insert into user_images (user_id, file_name, created_at, updated_at)
+	stmt = `insert into user_images (user_id, file_name, created_at, updated_at)
 		values ($1, $2, $3, $4) returning id`
-	err := m.DB.QueryRowContext(ctx, stmt,
+	err = m.DB.QueryRowContext(ctx, stmt,
 		i.UserID,
 		i.FileName,
 		time.Now(),
