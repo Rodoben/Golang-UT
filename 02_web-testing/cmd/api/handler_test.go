022ccfd9f6632a29a0ebade400f829525023a580
Ronald Benjamin <dodo.ben.rb@gmail.com>
2023-12-12 15:35:05 +0530

    refresh-token test case

--- 02_web-testing/cmd/api/handler_test.go
+++ 02_web-testing/cmd/api/handler_test.go
@@ -3,8 +3,11 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"strings"
 	"testing"
+	"time"
+	"web-testing/pkg/data"
 )
 
 func Test_app_Authentication(t *testing.T) {
@@ -32,5 +35,51 @@ func Test_app_Authentication(t *testing.T) {
 			t.Errorf("%s: authentication failed: expected status code: %d, but got: %d ", test.name, test.expectedCode, rr.Code)
 		}
 	}
+}
 
+func Test_app_RefreshToken(t *testing.T) {
+	var tests = []struct {
+		name               string
+		token              string
+		expectedCodeStatus int
+		resetRefreshTime   bool
+	}{
+		{name: "valid", token: "", expectedCodeStatus: http.StatusOK, resetRefreshTime: true},
+		{name: "expired token", token: expiredToken, expectedCodeStatus: http.StatusBadRequest, resetRefreshTime: false},
+		{name: "valid but about to expire", token: "", expectedCodeStatus: http.StatusTooEarly, resetRefreshTime: false},
+	}
+
+	testUser := data.User{
+		ID:        1,
+		FirstName: "Admin",
+		LastName:  "User",
+		Email:     "admin@example.com",
+	}
+	oldRefreshTime := refreshTokenExpiry
+	for _, test := range tests {
+		var tkn string
+		if test.token == "" {
+			if test.resetRefreshTime {
+				refreshTokenExpiry = time.Second * 1
+			}
+			tokens, _ := app.generateTokenPair(&testUser)
+			tkn = tokens.RefreshToken
+		} else {
+			tkn = test.token
+		}
+
+		postedData := url.Values{"refresh_token": {tkn}}
+
+		req, _ := http.NewRequest("POST", "/refresh-token", strings.NewReader(postedData.Encode()))
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		rr := httptest.NewRecorder()
+
+		handlertoTest := http.HandlerFunc(app.refreshToken)
+		handlertoTest.ServeHTTP(rr, req)
+
+		if rr.Code != test.expectedCodeStatus {
+			t.Errorf("%s: expected status code %d, but got %d", test.name, test.expectedCodeStatus, rr.Code)
+		}
+		refreshTokenExpiry = oldRefreshTime
+	}
 }
--- 02_web-testing/cmd/api/middleware_test.go
+++ 02_web-testing/cmd/api/middleware_test.go
@@ -1,1 +1,76 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"web-testing/pkg/data"
+)
+
+func TestEnableCors(t *testing.T) {
+	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+	tests := []struct {
+		name           string
+		method         string
+		expectedHeader bool
+	}{
+		{"get", "GET", false},
+		{"preflight", "OPTIONS", true},
+	}
+
+	for _, v := range tests {
+		handlerToTest := app.enableCors(nextHandler)
+		req, _ := http.NewRequest(v.method, "http://test", nil)
+		rr := httptest.NewRecorder()
+		handlerToTest.ServeHTTP(rr, req)
+
+		if v.expectedHeader && rr.Header().Get("Access-Control-Allow-Credentials") == "" {
+			t.Errorf("%s: expected Header, but did not find it", v.name)
+		}
+		if !v.expectedHeader && rr.Header().Get("Access-Control-Allow-Credentials") != "" {
+			t.Errorf("%s: expected no header, but got one", v.name)
+		}
+	}
+
+}
+
+func TestAuthRequired(t *testing.T) {
+	user := data.User{
+		ID:        1,
+		FirstName: "Admin",
+		LastName:  "User",
+		Email:     "Admin@example.com",
+	}
+
+	token, _ := app.generateTokenPair(&user)
+
+	nexthandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+	tests := []struct {
+		name             string
+		token            string
+		expectAuthorized bool
+		setHeader        bool
+	}{
+		{name: "valid token", token: fmt.Sprintf("Bearer %s", token.Token), expectAuthorized: true, setHeader: true},
+		{name: "no token", token: "", expectAuthorized: false, setHeader: false},
+		{name: "invalid token", token: fmt.Sprintf("Bearer %s", expiredToken), expectAuthorized: false, setHeader: true},
+	}
+
+	for _, test := range tests {
+		req, _ := http.NewRequest("GET", "http://test", nil)
+		if test.setHeader {
+			req.Header.Set("Authorization", test.token)
+		}
+		rr := httptest.NewRecorder()
+		handler := app.authRequired(nexthandler)
+		handler.ServeHTTP(rr, req)
+		fmt.Println("_", rr.Code)
+		if test.expectAuthorized && rr.Code == http.StatusUnauthorized {
+			t.Errorf("%s: got code 401, and should not have", test.name)
+		}
+		if !test.expectAuthorized && rr.Code != http.StatusUnauthorized {
+			t.Errorf("%s: got code 401, and should not have", test.name)
+		}
+	}
+}
