dbe12bc7e8ca8345ef0c09821b787c20ca0c38a6
Ronald Benjamin <dodo.ben.rb@gmail.com>
2024-01-08 22:00:56 +0530

    main ginkgo

--- 03_Test_driven_development/03_set_gingko_gomega/main.go
+++ 03_Test_driven_development/03_set_gingko_gomega/main.go
@@ -1,2 +1,17 @@
-package set
+package main
 
+import (
+	"fmt"
+	set "tdd/03_set_gingko_gomega/set"
+)
+
+func main() {
+	s := set.NewSet()
+	fmt.Println(s)
+	fmt.Println(s.IsEmpty())
+	fmt.Println(s.Size())
+	fmt.Println(s.Contains("red"))
+	s.Add("red")
+	fmt.Println(s.Contains("red"))
+	fmt.Println(s.Size())
+}
