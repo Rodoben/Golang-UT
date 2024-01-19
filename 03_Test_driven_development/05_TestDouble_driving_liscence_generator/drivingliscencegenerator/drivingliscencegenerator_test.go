f48ef66f4ed05e6e5ada394fc024f8ad86a92bf5
Ronald Benjamin <dodo.ben.rb@gmail.com>
2024-01-19 13:47:29 +0530

    main.go

--- 03_Test_driven_development/05_TestDouble_driving_liscence_generator/drivingliscencegenerator/drivingliscencegenerator_test.go
+++ 03_Test_driven_development/05_TestDouble_driving_liscence_generator/drivingliscencegenerator/drivingliscencegenerator_test.go
@@ -1,76 +1,10 @@
 package drivingliscencegenerator
 
 import (
-	"fmt"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
 )
-
-type (
-	UnderAgeApplicant       struct{}
-	LiscenceHolderApplicant struct{}
-	FakeRand                struct{}
-	ValidApplicant          struct {
-		initials string
-		dob      string
-	}
-	SpyLogger struct {
-		Callcount   int
-		Lastmessage string
-	}
-)
-
-func (u UnderAgeApplicant) IsAbove18() bool {
-	return false
-}
-
-func (u UnderAgeApplicant) HoldsLiscence() bool {
-	return false
-}
-func (u UnderAgeApplicant) GetInitials() string {
-	return ""
-}
-func (u UnderAgeApplicant) GetDOB() string {
-	return ""
-}
-
-func (l LiscenceHolderApplicant) IsAbove18() bool {
-	return true
-}
-
-func (l LiscenceHolderApplicant) HoldsLiscence() bool {
-	return true
-}
-func (l LiscenceHolderApplicant) GetInitials() string {
-	return ""
-}
-func (l LiscenceHolderApplicant) GetDOB() string {
-	return ""
-}
-func (v ValidApplicant) IsAbove18() bool {
-	return true
-}
-
-func (v ValidApplicant) HoldsLiscence() bool {
-	return false
-}
-func (v ValidApplicant) GetInitials() string {
-	return v.initials
-}
-func (v ValidApplicant) GetDOB() string {
-	return v.dob
-}
-
-func (s *SpyLogger) LogStuff(v string) {
-	s.Callcount++
-	s.Lastmessage = v
-}
-func (f FakeRand) GetRandomNumbers(len int) string {
-	fmt.Println(strings.Repeat("0", len))
-	return strings.Repeat("0", len)
-}
 
 type DrivingLiscenceSuite struct {
 	suite.Suite
