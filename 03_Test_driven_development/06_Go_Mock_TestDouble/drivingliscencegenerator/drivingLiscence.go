a63f2725301dbf5455cce28f5d9132817517fd8f
Ronald Benjamin <dodo.ben.rb@gmail.com>
2024-01-23 02:09:33 +0530

    gomock tdd testdouble

--- 03_Test_driven_development/06_Go_Mock_TestDouble/drivingliscencegenerator/drivingLiscence.go
+++ 03_Test_driven_development/06_Go_Mock_TestDouble/drivingliscencegenerator/drivingLiscence.go
@@ -1,15 +1,7 @@
 package drivingliscencegenerator
 
-import (
-	"fmt"
-	"strings"
-)
-
 type (
-	UnderAgeApplicant       struct{}
-	LiscenceHolderApplicant struct{}
-	FakeRand                struct{}
-	ValidApplicant          struct {
+	ValidApplicant struct {
 		initials string
 		dob      string
 	}
@@ -19,33 +11,6 @@ type (
 	}
 )
 
-func (u UnderAgeApplicant) IsAbove18() bool {
-	return false
-}
-
-func (u UnderAgeApplicant) HoldsLiscence() bool {
-	return false
-}
-func (u UnderAgeApplicant) GetInitials() string {
-	return ""
-}
-func (u UnderAgeApplicant) GetDOB() string {
-	return ""
-}
-
-func (l LiscenceHolderApplicant) IsAbove18() bool {
-	return true
-}
-
-func (l LiscenceHolderApplicant) HoldsLiscence() bool {
-	return true
-}
-func (l LiscenceHolderApplicant) GetInitials() string {
-	return ""
-}
-func (l LiscenceHolderApplicant) GetDOB() string {
-	return ""
-}
 func (v ValidApplicant) IsAbove18() bool {
 	return true
 }
@@ -63,8 +28,4 @@ func (v ValidApplicant) GetDOB() string {
 func (s *SpyLogger) LogStuff(v string) {
 	s.Callcount++
 	s.Lastmessage = v
-}
-func (f FakeRand) GetRandomNumbers(len int) string {
-	fmt.Println(strings.Repeat("0", len))
-	return strings.Repeat("0", len)
 }
--- 03_Test_driven_development/06_Go_Mock_TestDouble/drivingliscencegenerator/drivingliscencegenerator_test.go
+++ 03_Test_driven_development/06_Go_Mock_TestDouble/drivingliscencegenerator/drivingliscencegenerator_test.go
@@ -1,10 +1,32 @@
 package drivingliscencegenerator
 
 import (
+	"tdd/06_Go_Mock_TestDouble/drivingliscencegenerator/mocks"
 	"testing"
 
+	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/suite"
 )
+
+var (
+	ctrl *gomock.Controller
+	a    *mocks.MockDrivingLiscenceApplicants
+	l    *mocks.MockLogger
+	r    *mocks.MockRandomNumberGenerator
+	lg   *DrivingLiscenceNumberGenerator
+)
+
+func (s *DrivingLiscenceSuite) SetupTest() {
+	ctrl = gomock.NewController(s.T())
+	a = mocks.NewMockDrivingLiscenceApplicants(ctrl)
+	l = mocks.NewMockLogger(ctrl)
+	r = mocks.NewMockRandomNumberGenerator(ctrl)
+	lg = NewDrivingLiscenceNumberGenerator(l, r)
+}
+
+func (s *DrivingLiscenceSuite) TearDownTest() {
+	ctrl.Finish()
+}
 
 type DrivingLiscenceSuite struct {
 	suite.Suite
@@ -15,46 +37,42 @@ func TestDrivingLiscenceSuite(t *testing.T) {
 }
 
 func (s *DrivingLiscenceSuite) TestUnderAgeApplicant() {
-	a := UnderAgeApplicant{}
-	l := &SpyLogger{}
-	r := FakeRand{}
-	lg := NewDrivingLiscenceNumberGenerator(l, r)
+	a.EXPECT().IsAbove18().Return(false)
+	a.EXPECT().HoldsLiscence().Return(false)
+	l.EXPECT().LogStuff("Underaged Applicant, you must be 18 to hold liscence").Times(1)
 	_, err := lg.Generate(a)
 	s.Error(err)
 	s.Contains(err.Error(), "Underaged")
 
-	s.Equal(1, l.Callcount)
-	s.Contains(l.Lastmessage, "Underaged")
 }
 
 func (s *DrivingLiscenceSuite) TestNoSecondLiscence() {
-	a := LiscenceHolderApplicant{}
-	l := &SpyLogger{}
-	r := FakeRand{}
-	lg := NewDrivingLiscenceNumberGenerator(l, r)
+	a.EXPECT().HoldsLiscence().Return(true)
+	l.EXPECT().LogStuff("Duplicate Applicant, you can only hold one liscence").Times(1)
 	_, err := lg.Generate(a)
 	s.Error(err)
 	s.Contains(err.Error(), "Duplicate")
-
-	s.Equal(1, l.Callcount)
-	s.Contains(l.Lastmessage, "Duplicate")
 }
 
 func (s *DrivingLiscenceSuite) TestLiscenceGenerator() {
-	a := ValidApplicant{initials: "JH", dob: "07051997"}
-	l := &SpyLogger{}
-	r := FakeRand{}
+	a.EXPECT().HoldsLiscence().Return(false)
+	a.EXPECT().IsAbove18().Return(true)
+
+	a.EXPECT().GetInitials().Return("JH")
+	a.EXPECT().GetDOB().Return("07051997")
+	r.EXPECT().GetRandomNumbers(gomock.Any()).Return("00000")
+
 	lg := NewDrivingLiscenceNumberGenerator(l, r)
 	ln, err := lg.Generate(a)
 
 	s.NoError(err)
-	s.Equal("JH07051997000000", ln)
+	s.Equal("JH0705199700000", ln)
 
 }
 func (s *DrivingLiscenceSuite) TestLiscenceGeneratorShorterInitials() {
 	a := ValidApplicant{initials: "JH", dob: "07051997"}
 	l := &SpyLogger{}
-	r := FakeRand{}
+	//r := FakeRand{}
 	lg := NewDrivingLiscenceNumberGenerator(l, r)
 	ln, err := lg.Generate(a)
 
